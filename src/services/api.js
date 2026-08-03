fa2ca88df539b22956e61af25c31705072711f03
Sinto-Antony <sintoantony295@gmail.com>
2026-08-03 17:23:20 +0530

    Connect frontend to production API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://applyflow-api-czyy.onrender.com/api",
 });
 
-export default api;
+export default api;;
